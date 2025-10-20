91476dcb688b294e60b77582bb47734f00c2bfdb
Diego Pérez Rodríguez <diegoperez.developer@gmail.com>
2025-10-20 13:53:01 +0200

    new art pieces

--- scripts.js
+++ scripts.js
@@ -157,6 +157,18 @@ function setupGalleryModal() {
     });
 }
 
+/* ---------- GOOGLE TRANSLATE ---------- */
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {
+            pageLanguage: 'de',
+            includedLanguages: 'en,fr,es,it,de',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        },
+        'google_translate_element'
+    );
+}
+
 
 /* ---------- BOOT ---------- */
 $(document).ready(function () {
