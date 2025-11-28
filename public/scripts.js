06782ddb45bd98fb005ffb1e5c42dafed9f8a503
Diego Pérez Rodríguez <diegoperez.developer@gmail.com>
2025-11-28 22:55:47 +0100

    remove google translator plugging

--- public/scripts.js
+++ public/scripts.js
@@ -157,17 +157,7 @@ function setupGalleryModal() {
     });
 }
 
-/* ---------- GOOGLE TRANSLATE ---------- */
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement(
-        {
-            pageLanguage: 'de',
-            includedLanguages: 'en,fr,es,it,de',
-            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-        },
-        'google_translate_element'
-    );
-}
+
 
 
 /* ---------- PAGINATION ---------- */
